ee944fc59e962063fab912eebe1585bd8092303d
MrWaithira <waithira.pius@gmail.com>
2022-09-10 18:03:13 -0700

    tr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- *
  * main - function
  *
- * Return 0 (successful)
+ * Return: Always 0 (Successful)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
