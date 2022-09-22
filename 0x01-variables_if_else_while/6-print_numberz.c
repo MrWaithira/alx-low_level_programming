3b21adbedba58cfbf2951a7265bbe32a0c8406d6
MrWaithira <waithira.pius@gmail.com>
2022-09-22 02:41:39 -0700

    6-print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - code
+ *
+ * Return: Always 0 (if success, then true)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
