911eb854710ef04c2a5e0609a2862d0560de990e
MrWaithira <waithira.pius@gmail.com>
2022-10-05 23:50:46 -0700

    changed name copied original to dummy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * main - memset
- *
- * Return: Always 0 (success)
- */
-int _putchar(char);
-char *_memset(char *s, char b, unsigned int n);
+#include "main.h"
 
-#endif /*MAIN_H*/
+/**
+ * _memset - fills a memory block with a constant byte
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: number of bytes to be used
+ *
+ * Return: pointer to the memory block
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/0-memsets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - memset
+ *
+ * Return: Always 0 (success)
+ */
+int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /*MAIN_H*/
