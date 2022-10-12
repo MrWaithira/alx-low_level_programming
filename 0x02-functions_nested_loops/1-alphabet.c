329d25880a29a674dd20f024ddcf691b87c31211
MrWaithira <56521646+MrWaithira@users.noreply.github.com>
2022-10-12 19:34:14 +0300

    0x02 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - functions nested loops
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int j;
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int t;
+	t = (12 % 2);
+
+	printf("the results of the fucntion is %d.\n", t);
+	return (0);
+}
