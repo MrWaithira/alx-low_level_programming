11b3a7d4ccde39b40b6383a5e4cae31da16cb32e
MrWaithira <56521646+MrWaithira@users.noreply.github.com>
2022-10-12 19:44:22 +0300

    0x02 1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void  print_alphabet(void);
-print_alphabet_x10(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int islower(int c);
 int _putchar(char c);
 int _isalpha(int c);
