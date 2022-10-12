cbb775fc337857d24f35031c1f32996f85231c74
MrWaithira <56521646+MrWaithira@users.noreply.github.com>
2022-10-12 20:44:28 +0300

    1-alphabet changed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: ALways 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	}
+	_putchar('\n');
+}
